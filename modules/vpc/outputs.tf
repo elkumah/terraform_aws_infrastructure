21cb4ca104b0d8f2b3d2700db54c5beffc9f47d6
elkumah <e.fkumah@gmail.com>
2026-06-08 22:31:58 +0100

    Feat: Added examples/vpc folder to document how the module is used

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,5 +1,21 @@
 # modules/vpc/outputs.tf
-output "vpc_id"             { value = aws_vpc.this.id }
-output "public_subnet_ids"  { value = aws_subnet.public[*].id }
-output "private_subnet_ids" { value = aws_subnet.private[*].id }
-output "vpc_cidr_block"     { value = aws_vpc.this.cidr_block }
+output "vpc_id" { 
+ description = "The ID of the VPC"
+ value = aws_vpc.this.id 
+ }
+output "public_subnet_ids"  { 
+    description = "The IDs of the public subnets"
+    value = aws_subnet.public[*].id 
+}
+output "private_subnet_ids" {
+     description = "The IDs of the private subnets"
+     value = aws_subnet.private[*].id 
+}  
+output "vpc_cidr_block" { 
+    description = "The CIDR block of the VPC"
+    value = aws_vpc.this.cidr_block
+}
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.this.id
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,24 +1,20 @@
 variable "vpc_cidr_block" {
     description = "The CIDR block for the VPC"
     type = string
-    default = "10.0.0.0/16"
 
 }
 
 variable "public_subnet_cidrs" {
     description = "The list of CIDR blocks for the public subnet"
     type = list(string)
-    default = ["10.0.10.0/24", "10.0.11.0/24"]
 }
 variable "private_subnet_cidrs" {
     description = "The list of CIDR blocks for the private subnet"
     type = list(string)
-    default = ["10.0.10.0/24", "10.0.11.0/24"]
 }
 variable "availability_zones"{
     description = "The list of availability zones for the VPC"
     type = list(string)
-    default = ["us-east-1a", "us-east-1b"]
 }
 
 variable "enable_nat_gateway" {
