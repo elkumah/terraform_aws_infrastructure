391a068777019429207c05d5f1949207460e0642
elkumah <e.fkumah@gmail.com>
2026-06-09 10:55:43 +0100

    Modified: Added module to create an Ec2 instance with the latest Ubuntu AMI

---
+++ modules/ec2/main.tf
@@ -0,0 +1,66 @@
+# This module creates an EC2 instance with the latest Ubuntu 24.04 AMI.
+# Dynamically retrieves the latest Ubuntu 24.04 AMI using the aws_ami data source.
+data "aws_ami" "ubuntu_24_04" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-24.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  
+}
+# Security group to allow ingress traffic based on provided rules.
+resource "aws_security_group" "ec2_sg" {
+  name        = "${var.name_prefix}-ec2-sg"
+  description = "Security group for EC2 instance"
+  vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  tags = {
+    Name = "${var.name_prefix}-ec2-sg"
+  }
+}
+# Provision the EC2 instance using the retrieved AMI ID and provided variables.
+resource "aws_instance" "ec2_instance" {
+  ami                    = data.aws_ami.ubuntu_24_04.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  user_data              = var.user_data
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  subnet_id              = var.subnet_id
+  associate_public_ip_address = var.associated_public_ip
+
+  root_block_device {
+    volume_size = var.root_volume_size
+    volume_type = "gp2"
+    encrypted   = true
+    delete_on_termination = true
+  }
+
+  metadata_options {
+    http_tokens = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-ec2-instance"
+  }
+}
